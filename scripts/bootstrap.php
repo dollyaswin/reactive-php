e212748bd30066311b086072c750e5cfc94c8bb1
Dolly Aswin Harahap <dolly.aswin@gmail.com>
2018-04-27 01:29:37 +0800

    Add bootstrap for example

---
+++ scripts/bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+require __DIR__ . '/../vendor/autoload.php';
+
+use React\EventLoop\Factory;
+use Rx\Scheduler;
+
+$loop = Factory::create();
+//You only need to set the default scheduler once
+Scheduler::setDefaultFactory(function () use ($loop) {
+    return new Scheduler\EventLoopScheduler($loop);
+});

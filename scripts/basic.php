bdc039dd3be6ad3cdb7da54be7eb2d0cb3b72077
Dolly Aswin Harahap <dolly.aswin@gmail.com>
2018-04-27 01:32:30 +0800

    Remove default scheeduler, just use bootstrap

--- scripts/basic.php
+++ scripts/basic.php
@@ -1,14 +1,6 @@
 <?php
+
 require_once __DIR__ . '/bootstrap.php';
-
-use React\EventLoop\Factory;
-use Rx\Scheduler;
-
-$loop = Factory::create();
-//You only need to set the default scheduler once
-Scheduler::setDefaultFactory(function () use ($loop) {
-    return new Scheduler\EventLoopScheduler($loop);
-});
 
 $fruits = ['apple', 'banana', 'orange', 'raspberry'];
 $source = \Rx\Observable::fromArray($fruits);

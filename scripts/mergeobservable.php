09e63ab8bd463bd378c9fc01bbde30f6f2b878ab
Dolly Aswin <dolly.aswin@gmail.com>
2018-04-28 05:13:58 +0000

    Add merge() operator

---
+++ scripts/mergeobservable.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . '/bootstrap.php';
+
+use Rx\Observer\CallbackObserver;
+use Rx\Observable;
+
+$fruits = ['apple', 'banana', 'orange', 'raspberry'];
+$vegetables = ['potato', 'carrot'];
+
+$observer = new CallbackObserver(
+    function ($value) {
+        echo "Next: $value\n";
+    },
+    function (\Exception $err) {
+        $msg = $err->getMessage();
+        echo "Error: $msg\n";
+    },
+    function () {
+        echo "Complete\n";
+    }
+);
+
+Observable::fromArray($fruits)
+    ->map(function ($value) {
+        return strlen($value);
+    })
+    ->filter(function ($len) {
+        return $len > 5;
+    })
+    ->merge(Observable::fromArray($vegetables))
+    ->subscribe($observer);
+
+$loop->run();

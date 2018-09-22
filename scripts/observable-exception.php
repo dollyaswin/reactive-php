ffc4a0c7acabc7affc98ba68cf02c03d6ba9525b
Dolly Aswin <dolly.aswin@gmail.com>
2018-09-22 22:29:29 +0700

    Add Observable with throwing Exception

---
+++ scripts/observable-exception.php
@@ -0,0 +1,33 @@
+<?php
+
+require_once __DIR__ . '/bootstrap.php';
+
+use Rx\Observer\CallbackObserver;
+use Rx\Observable;
+
+$fruits   = ['grape', 'watermelon', 'strawberry', 'pineaple'];
+$observer = new CallbackObserver(
+    function ($value) {
+        printf("Next: %d chars" . PHP_EOL, $value);
+    },
+    function (\Exception $err) {
+        printf("Error: %s" . PHP_EOL, $err->getMessage());
+    },
+    function () {
+        echo "Complete" . PHP_EOL;
+    }
+);
+
+Observable::fromArray($fruits)
+    ->map(function ($value) {
+    	if (strpos($value, 'berry') !== false) {
+    		throw new \Exception('Fruits with *berry rejected!');
+    	}
+        return strlen($value);
+    })
+    ->filter(function ($len) {
+        return $len > 5;
+    })
+    ->subscribe($observer);
+
+$loop->run();

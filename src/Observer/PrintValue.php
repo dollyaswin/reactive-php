d79abc1e0f653bd703f5a3fe1f6e560a0ff081f8
Dolly Aswin <dolly.aswin@gmail.com>
2018-09-22 22:28:34 +0700

    Add Observer\PrintValue

---
+++ src/Observer/PrintValue.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace RxExample\Observer;
+
+class PrintValue extends \Rx\Observer\AbstractObserver
+{
+    protected function completed()
+    {
+        print("Completed" . PHP_EOL);
+    }
+    protected function next($item)
+    {
+        printf("Next: %s" . PHP_EOL, $item);
+    }
+    protected function error(\Throwable $err)
+    {
+        printf("Error: %s" . PHP_EOL, $err->getMessage());
+    }
+}

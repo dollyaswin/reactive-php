2d9fde04d80ba143b371bdc23d5f28e0371fbf14
Dolly Aswin <dolly.aswin@gmail.com>
2018-09-23 07:36:01 +0700

    Add singles example usage

---
+++ scripts/singles.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/bootstrap.php';
+
+use Rx\Observable;
+use RxExample\Observer\PrintValue;
+
+Observable::just('100')
+    ->subscribe(new PrintValue);
+$loop->run();
